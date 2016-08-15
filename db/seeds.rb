b3b324dcdcb177fc93fdff1424a0879bf8f60682
Chen Yang <yangc5@mail.uc.edu>
2016-08-14 19:28:39 -0500

    using seeds to generate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+350000.times do |i|
+  Dog.create!(
+  name: Faker::Name.name,
+  gender: ['male', 'female'].sample,
+  age: rand(0..15),
+  breed: ['Border Terrier', 'Boxer', 'Cavalier King Charles Spaniel', 'English Cocker Spaniel', 'English Springer Spaniel', 'German Shepherd', 'Golden Retriever', 'Labrador Retriever', 'Staffordshire Bull Terrier', 'West Highland White Terrier'].sample
+  )
+end
